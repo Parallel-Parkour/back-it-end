5b68c4548d7b75b8793606d0b40991036006851d
Laurel Perkins <lmperkins20@gmail.com>
2023-05-04 12:47:52 -0700

    changed chalk installation requirements. Install chalk@4.1.2 per package.json

--- auth/auth.test.js
+++ auth/auth.test.js
@@ -1,11 +1,13 @@
 'use strict';
+
 const { createUser } = require('./createUser');
+const chalk = require('chalk');
 
 describe('Testing the createUser function', () => {
   test('Can successfully create a new user and assign roles', () => {
     let username = 'babyKirk';
     let password = 'babyKirkIsBaby';
     createUser(username, password);
-    expect(console.log()).toEqual('User created: babyKirk');
+    expect(console.log()).toEqual(chalk.magenta('User created: babyKirk'));
   });
 });
