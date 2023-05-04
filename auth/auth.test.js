77c81cd1a987d9889cb26b59b4e930be22e9268c
Laurel Perkins <lmperkins20@gmail.com>
2023-05-04 13:19:54 -0700

    added test for assigning roles to new user

--- auth/auth.test.js
+++ auth/auth.test.js
@@ -4,11 +4,32 @@ const { createUser } = require('./createUser');
 const chalk = require('chalk');
 
 describe('Testing the createUser function', () => {
+
   test('Can successfully create a new user', () => {
+
+    const logSpy = jest.spyOn(global.console, 'log').mockImplementation();
+
     let username = 'babyKirk';
     let password = 'babyKirkIsBaby';
+
     createUser(username, password);
-    expect(console.log()).toEqual(chalk.magenta('User created: babyKirk'));
-    // why is it giving me prompts tho?
+
+    expect(logSpy).toHaveBeenCalledWith('User created: babyKirk');
+
+    logSpy.mockRestore();
+  });
+
+  test('Can successfully assign roles to a new user', () => {
+
+    const logSpy = jest.spyOn(global.console, 'log').mockImplementation();
+
+    let username = 'superBabyKirk';
+    let password = 'babyKirkIsVerySuperBaby';
+
+    createUser(username, password);
+
+    expect(logSpy).toHaveBeenCalledWith('Roles and permissions assigned successfully.');
+
+    logSpy.mockRestore();
   });
 });
