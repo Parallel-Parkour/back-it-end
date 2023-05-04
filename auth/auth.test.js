e9db94a895214b53f597ddc704f170e5e23c5b08
Laurel Perkins <lmperkins20@gmail.com>
2023-05-04 12:51:57 -0700

    fixed typos

--- auth/auth.test.js
+++ auth/auth.test.js
@@ -4,10 +4,11 @@ const { createUser } = require('./createUser');
 const chalk = require('chalk');
 
 describe('Testing the createUser function', () => {
-  test('Can successfully create a new user and assign roles', () => {
+  test('Can successfully create a new user', () => {
     let username = 'babyKirk';
     let password = 'babyKirkIsBaby';
     createUser(username, password);
     expect(console.log()).toEqual(chalk.magenta('User created: babyKirk'));
+    // why is it giving me prompts tho?
   });
 });
